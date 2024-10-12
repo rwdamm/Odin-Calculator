f66e84f757e1e6672eb44ced6d7bfc67a9935ec2
rwdamm <66540194+rwdamm@users.noreply.github.com>
2024-10-12 15:08:14 -0400

    added operation functions. added equal button and function. added equalClick variable to track status of operations and have the calculator respond to clicks in expected ways.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//initialze display value
-const total = 0;
+//initialize display value
+Total = 0;
+Value1 = 0;
+Value2 = 0;
+operation = "clear";
+equalClick = 0
 
 function add(a,b){
     return a + b;
@@ -16,14 +20,16 @@ function divide(a,b){
 
 function operate (operator, a, b){
     switch (operator){
-        case "+":
+        case "add":
             return add(a,b);
-        case "-":
+        case "subtract":
             return subtract(a,b);
-        case "*":
+        case "multiply":
             return multiply(a,b);
-        case "/":
+        case "divide":
             return divide(a,b);
+        case "clear":
+            return a;
     }
     
 }
@@ -36,3 +42,62 @@ function updateDisplay(update){
     display.appendChild(displayValue);
 
 }
+
+function clearCalculator(){
+    updateDisplay("");
+    Total = 0;
+    Value1 = 0;
+    Value2 = 0;
+    equalClick = 0;
+    operation = "clear";
+}
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((numberButton)=>{
+    numberButton.addEventListener("click", ()=>{
+        console.info(numberButton.id);
+        if(equalClick > 0){
+            clearCalculator();
+        }
+        if(operation === "clear"){
+        Value1 = Value1 * 10 + (numberButton.id * 1);
+        console.info("Value1= " + Value1);
+        updateDisplay(Value1);
+        return;
+        }
+        else{
+            Value2 = Value2 * 10 + (numberButton.id * 1);
+            console.info("Value2= " + Value2);
+            updateDisplay(Value2);
+        }
+    })
+})
+
+const operateButtons = document.querySelectorAll(".operate");
+operateButtons.forEach((operateButton)=>{
+    operateButton.addEventListener("click", ()=>{
+        console.info("ID = " + operateButton.id);
+        operation = operateButton.id
+        Value2 = 0;
+        equalClick = 0;
+        console.info("operation = " + operation);
+    })
+});
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", () => {
+    Total = operate(operation, Value1, Value2);
+    updateDisplay(Total);
+    Value1 = Total;
+    equalClick++;
+});
+
+const clearAllButton = document.querySelector(".clearAll");
+clearAllButton.addEventListener("click", () => {
+    updateDisplay("");
+    Total = 0;
+    Value1 = 0;
+    Value2 = 0;
+    equalClick = 0;
+    operation = "clear";
+});
